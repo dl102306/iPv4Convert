1cd0fc44c603bf6dde7f6fa422773b0e13760c0a
dl102306 <tjdl102306@gmail.com>
2018-11-25 21:38:07 +0800

    confict - release 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function ipv4Convert(ipv) {
   const masks = [];
   const len = ipv.length + 1;
   let value = '';
-  let gittest = 'confilct1';
+  let gittest = 'confilct-release1.0-2';
 
   ipv = ipv + '.';
   for (let i = 0; i < len; i++) {
